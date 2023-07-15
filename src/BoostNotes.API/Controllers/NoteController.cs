ed2f01b711b405945077cf47bddb051b4cb6479e
Adam Stirtan <adamstirtan@gmail.com>
2023-07-15 10:40:40 -0400

    Note CRUD

---
+++ src/BoostNotes.API/Controllers/NoteController.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using BoostNotes.Data.Contexts;
+using BoostNotes.Domain.Entities;
+
+namespace BoostNotes.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NoteController : ControllerBase
+{
+    private readonly ILogger<NoteController> _logger;
+    private readonly BoostNotesDbContext _context;
+    
+    public NoteController(
+        ILogger<NoteController> logger,
+        BoostNotesDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_context.Notes);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Note note)
+    {
+        await _context.Notes.AddAsync(note);
+        await _context.SaveChangesAsync();
+
+        return Ok(note);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put([FromRoute] long id, [FromBody] Note note)
+    {
+        _context.Update(note);
+
+        await _context.SaveChangesAsync();
+
+        return Ok(note);
+    }
+
+    [HttpDelete("id")]
+    public async Task<IActionResult> Delete([FromRoute] long id)
+    {
+        var note = await _context.Notes.SingleOrDefaultAsync(x => x.Id == id);
+
+        if (note == null)
+        {
+            return NotFound();
+        }
+
+        _context.Notes.Remove(note);
+
+        await _context.SaveChangesAsync();
+
+        return Ok(note);
+    }
+}
