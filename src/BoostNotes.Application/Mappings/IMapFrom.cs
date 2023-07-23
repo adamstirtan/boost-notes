e18e6a021ee0999d19e0fd0f43a523e29acace30
Adam Stirtan <adamstirtan@gmail.com>
2023-07-22 22:14:30 -0400

    Setup AutoMapper

---
+++ src/BoostNotes.Application/Mappings/IMapFrom.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace BoostNotes.Application.Mappings
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
